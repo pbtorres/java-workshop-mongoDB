70b056652ffd3bae1b2a71ed17d6fe9b173fecd4
Pablo Torres <pablotorreeifel@gmail.com>
2022-11-07 13:28:12 -0300

    Creatin entity Post with nested (aninhado) user

--- src/main/java/com/torres/workshop/config/Instantiation.java
+++ src/main/java/com/torres/workshop/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.torres.workshop.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.torres.workshop.domain.Post;
 import com.torres.workshop.domain.User;
+import com.torres.workshop.repository.PostRepository;
 import com.torres.workshop.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,29 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository; 
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		postRepository.deleteAll();
 		userRepository.deleteAll();
+		
 		
 		User maria = new User(null, "Maria", "maria@gmail.com");
 		User paulo = new User(null, "Paulo", "paulo@gmail.com");
 		User joao = new User(null, "João", "joao@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, paulo, joao));
+		
+		Post post1 = new Post(null, sdf.parse("07/11/2022"), "Partiu viagem", "Vou viajar para o Piauí.", maria);
+		Post post2 = new Post(null, sdf.parse("07/12/2022"), "Retorno da viagem", "Vou voltar para bsb.", joao);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
