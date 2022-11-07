885602b5d609c21b97cae8bfb7d844a88c703993
Pablo Torres <pablotorreeifel@gmail.com>
2022-11-07 16:49:57 -0300

    Referencing user posts

--- src/main/java/com/torres/workshop/config/Instantiation.java
+++ src/main/java/com/torres/workshop/config/Instantiation.java
@@ -32,7 +32,6 @@ public class Instantiation implements CommandLineRunner {
 		postRepository.deleteAll();
 		userRepository.deleteAll();
 		
-		
 		User maria = new User(null, "Maria", "maria@gmail.com");
 		User paulo = new User(null, "Paulo", "paulo@gmail.com");
 		User joao = new User(null, "João", "joao@gmail.com");
@@ -43,6 +42,10 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("07/12/2022"), "Retorno da viagem", "Vou voltar para bsb.", new AuthorDTO(joao));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		
+		userRepository.save(maria);
 		
 	}
 
